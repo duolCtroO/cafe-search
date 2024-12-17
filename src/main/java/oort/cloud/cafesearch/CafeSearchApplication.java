10deaa800ae7c47f96365b8611790c977caf276e
OortCloud <skintoodev@gmail.com>
2024-12-17 14:54:11 +0900

    project init

---
+++ src/main/java/oort/cloud/cafesearch/CafeSearchApplication.java
@@ -0,0 +1,13 @@
+package oort.cloud.cafesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oort/cloud/cafesearch/CafeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package oort.cloud.cafesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
