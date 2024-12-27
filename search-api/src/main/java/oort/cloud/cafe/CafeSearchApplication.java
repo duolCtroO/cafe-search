a70d7ceb163561aa37387f93a76949bc2d40d4c6
OortCloud <skintoodev@gmail.com>
2024-12-27 16:37:48 +0900

    S-3 각 모듈 통합

---
+++ search-api/src/main/java/oort/cloud/cafe/CafeSearchApplication.java
@@ -0,0 +1,14 @@
+package oort.cloud.cafe;
+
+import oort.cloud.cafe.feign.NaverFeignClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(clients = NaverFeignClient.class)
+public class CafeSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CafeSearchApplication.class, args);
+    }
+}
